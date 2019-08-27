636398417479a73e942cd6c6d85a9f91d06b36ff
Brian <brianjohnnychen@gmail.com>
2019-08-27 19:12:09 -0400

    Lesson 28 Next - Component Props

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -3,18 +3,52 @@ class Person {
         this.name = name
         this.age = age
     }
-    getGreeting() {
+    getDescription() {
         // return "Hello " + this.name
         // Backticks create template strings.
-        return `Hello, this is ${this.name}!`
-    }
-    getDescription() {
-        return ` I am ${this.age} years old.`
+        return `Hello, this is ${this.name}! I am ${this.age} years old.`
     }
 }
 
-const me = new Person("Brian Chen", 27)
-console.log(me.getGreeting())
-const you = new Person("", 18)
-console.log(you.getGreeting)
-console.log(you.getGreeting() + you.getDescription())
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major
+    }
+    hasMajor() {
+        return !!this.major
+    }
+    getDescription() {
+        let description = super.getDescription()
+        if(this.hasMajor) {
+            description += ` My major is ${this.major}.`
+        }
+        return description
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age)
+        this.homeLocation = homeLocation
+    }
+    hasHomeLocation() {
+        this.hasHomeLocation ? true: false
+    }
+    getDescription() {
+        let description = super.getDescription()
+        if(this.hasHomeLocation) {
+            description += ` I am from ${this.homeLocation}`
+        }
+        return description
+    }
+}
+
+const me = new Student("Brian Chen", 27, "CSE")
+console.log(me.getDescription())
+
+const other = new Student()
+console.log(other.getDescription())
+
+const traveler = new Traveler("Elaine", 28, "Tainan")
+console.log(traveler.getDescription())
