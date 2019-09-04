e297c6c82f7696f189d7161aa97634937d9bf5be
Brian Chen <brianjohnnychen@gmail.com>
2019-09-04 01:03:16 -0400

    Refactored using component states.

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,28 +1,60 @@
-const bodyText = {
-    text: "I am now visible (default)",
-    isVisible: true
-}
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props)
 
-const toggleVisibility = () => {
-    if(bodyText.isVisible) {
-        bodyText.isVisible = false
-        bodyText.text = ""
-    } else {
-        bodyText.isVisible = true
-        bodyText.text = "I am now visible (set by button)"
+        this.state = {
+            title: "Visibility Toggle",
+            visible: false
+        }
+
+        this.toggleVisibility = this.toggleVisibility.bind(this)
     }
-    renderApp()
-}
 
-const appRoot = document.getElementById("app")
-const renderApp = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>{bodyText.isVisible ? "Hide content" : "Show content"}</button>
-            <p>{bodyText.text}</p>
-        </div>
-    )
-    ReactDOM.render(template, appRoot)
+    toggleVisibility(props) {
+        this.setState((prevState) => {
+            return {
+                visible: !prevState.visible
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>{this.state.title}</h1>
+                <button onClick={this.toggleVisibility}>Toggle Visibility</button>
+                <p>{this.state.visible ? "Visibility On" : ""}</p>
+            </div>
+        )
+    }
 }
-renderApp()
+ReactDOM.render(<VisibilityToggle/>, document.getElementById("app"))
+
+// const bodyText = {
+//     text: "I am now visible (default)",
+//     isVisible: true
+// }
+
+// const toggleVisibility = () => {
+//     if(bodyText.isVisible) {
+//         bodyText.isVisible = false
+//         bodyText.text = ""
+//     } else {
+//         bodyText.isVisible = true
+//         bodyText.text = "I am now visible (set by button)"
+//     }
+//     renderApp()
+// }
+
+// const appRoot = document.getElementById("app")
+// const renderApp = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleVisibility}>{bodyText.isVisible ? "Hide content" : "Show content"}</button>
+//             <p>{bodyText.text}</p>
+//         </div>
+//     )
+//     ReactDOM.render(template, appRoot)
+// }
+// renderApp()
