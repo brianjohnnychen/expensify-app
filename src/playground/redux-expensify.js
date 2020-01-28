4457d49592be7418c5fb03ef26389df1c664b901
Brian Chen <brianchen@brians-mbp.lan>
2020-01-28 01:42:30 -0500

    Broke down redux-expensify into multiple folders and files.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -118,6 +118,23 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 }
 
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
+
+        // Return true if all 3 match, else remove item from visible.
+        return startDateMatch && endDateMatch && textMatch
+    }).sort((a, b) => {
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1
+        } else if (sortBy === 'amount'){
+            return a.amount < b.amount ? 1 : -1
+        }
+    })
+}
+
 // Store creation
 const store = createStore(
     combineReducers({
@@ -127,24 +144,27 @@ const store = createStore(
 )
 
 store.subscribe(() => {
-    console.log(store.getState())
+    const state = store.getState()
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses)
 })
 
-// const expenseOne = store.dispatch(addExpense({description: 'rent', amount: 100}))
-// const expenseTwo = store.dispatch(addExpense({description: 'coffee', amount: 200}))
+const expenseOne = store.dispatch(addExpense({description: 'rent', amount: 300, createdAt: -11000}))
+const expenseTwo = store.dispatch(addExpense({description: 'coffee', amount: 200, createdAt: -21000}))
 
 // store.dispatch(removeExpense({id: expenseOne.expense.id}))
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}))
 
-// store.dispatch(setTextFilter('new filter text'))
+// store.dispatch(setTextFilter('re'))
+// store.dispatch(setTextFilter('food'))
 
-// store.dispatch(sortByAmount())
-// store.dispatch(sortByDate())
+store.dispatch(sortByAmount(expenseOne, expenseTwo))
+store.dispatch(sortByDate(expenseOne, expenseTwo))
 
-store.dispatch(setStartDate(125))
-store.dispatch(setStartDate())
-store.dispatch(setEndDate(1250))
-store.dispatch(setEndDate())
+// store.dispatch(setStartDate(125))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(-125))
+// store.dispatch(setEndDate())
 
 const demoStore = {
     expenses: [{
