2d8d13fb5182745526ec6508b5c899bfeee1dc75
Brian Chen <brianchen@brians-mbp.lan>
2020-02-04 21:06:36 -0500

    Added expense form and form logic.

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ import './styles/styles.scss'
 
 const store = configureStore()
 
-store.dispatch(addExpense({description: 'water bill'}))
+store.dispatch(addExpense({description: 'water bill', amount: 4500}))
 store.dispatch(addExpense({description: 'gas bill'}))
-store.dispatch(setTextFilter('bill'))
+store.dispatch(addExpense({description: 'Rent', amount: 12500}))
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
