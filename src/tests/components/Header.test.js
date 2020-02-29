5650e080b379ea10c3fd01def74c54e1b9dfdcc4
Brian Chen <brianjohnnychen@gmail.com>
2020-02-28 23:52:21 -0500

    set up enzyme for jest testing

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
+// import ReactShallowRenderer from 'react-test-renderer/shallow'
 import Header from '../../components/Header'
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header/>)
-    expect(renderer.getRenderOutput()).toMatchSnapshot() // will always pass on first run b/c there is not existing snapshot
+    const wrapper = shallow(<Header/>)
+    expect(wrapper).toMatchSnapshot()
+
+    // expect(wrapper.find('h1').length).toBe(1)
+    // const renderer = new ReactShallowRenderer()
+    // renderer.render(<Header/>)
+    // expect(renderer.getRenderOutput()).toMatchSnapshot() // will always pass on first run b/c there is not existing snapshot
 })
 
 // react-test-renderer
