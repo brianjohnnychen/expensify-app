164c7bfa3a1d297edfd5c9fb54990c9dd518f47b
Brian Chen <brianchen@brians-mbp.lan>
2020-03-05 14:02:39 -0500

    Added remove expense and update expense functionality & test cases.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses'
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense, startEditExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
@@ -27,6 +27,22 @@ test("should setup remove expense action object", () => {
     })
 })
 
+test('should remove expense from Firebase', (done) => {
+    const store = createMockStore({})
+    const id = expenses[2].id
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        })
+        return database.ref(`expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val()).toBeFalsy()
+        done()
+    })
+})
+
 // editExpense
 test("should setup edit expense action object", () => {
     const action = editExpense("123abc", { note: "new note" })
@@ -36,6 +52,24 @@ test("should setup edit expense action object", () => {
         updates: {
             note: 'new note'
         }
+    })
+})
+
+test('should edit expense from Firebase', (done) => {
+    const store = createMockStore({})
+    const id = expenses[0].id
+    const updates = { amount: 2345 }
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        })
+        return database.ref(`expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount)
+        done()
     })
 })
 
