1ae56d6604e3bc4fe71c848c113f3fa127459447
Brian Chen <brianchen@brians-mbp.lan>
2020-03-05 12:14:12 -0500

    Get firebase expenses on start.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,11 +1,19 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
 // create configuration so test cases can create the same mock store
 const createMockStore = configureMockStore([thunk])
+
+beforeEach((done) => {
+    const expensesData = {}
+    expenses.forEach(({ id, description, note, amount, createdAt}) => {
+        expensesData[id] = { description, note, amount, createdAt }
+    })
+    database.ref('expenses').set(expensesData).then(() => done())
+})
 
 // removeExpense
 test("should setup remove expense action object", () => {
@@ -93,19 +101,22 @@ test('should add expense with defaults to database and store', (done) => {
     })
 })
 
-// test("should setup add expense action object with default values", () => {
-//     const expenseData = {
-//         description: '',
-//         amount: 0,
-//         createdAt: 0,
-//         note: ''
-//     }
-//     const action = addExpense(expenseData)
-//     expect(action).toEqual({
-//         type: 'ADD_EXPENSE',
-//         expense: {
-//             ...expenseData,
-//             id: expect.any(String)
-//         }
-//     })
-// })
+test('should setup SET_EXPENSE object with data', () => {
+    const action = setExpenses(expenses)
+    expect(action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    })
+})
+
+test('should fetch the expenses from Firebase', (done) => {
+    const store = createMockStore({})
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'SET_EXPENSES',
+            expenses
+        })
+        done()
+    })
+})
