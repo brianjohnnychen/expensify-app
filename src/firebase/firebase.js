2e03e1fb39d07e2009665fdd577a9445e4e62c19
Brian Chen <brianchen@brians-mbp.lan>
2020-03-05 01:51:26 -0500

    Created a separate test database and fixed related dependencies.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,16 +3,14 @@
 import * as firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyAFjH8kW34kRmVvv3MsDYu6pN6o8jQM3vc",
-    authDomain: "expensify-82443.firebaseapp.com",
-    databaseURL: "https://expensify-82443.firebaseio.com",
-    projectId: "expensify-82443",
-    storageBucket: "expensify-82443.appspot.com",
-    messagingSenderId: "365745866313",
-    appId: "1:365745866313:web:c6e26a5e2019816916a198",
-    measurementId: "G-E23FYDYSX5"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
-  
+
 // Initialize Firebase
 firebase.initializeApp(config)
 
