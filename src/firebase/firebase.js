e05b0bf4bf230cdd78ecee9bbec218c8f4eef71e
Brian Chen <brianchen@brians-mbp.lan>
2020-03-09 17:30:00 -0400

    Added private routes.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,7 +16,10 @@ firebase.initializeApp(config)
 
 const database = firebase.database()
 
-export { firebase, database as default}
+// Add authentication provider.
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+
+export { firebase, googleAuthProvider, database as default}
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { shallow } from 'enzyme'
 // import toJSON from 'enzyme-to-json'
 // import ReactShallowRenderer from 'react-test-renderer/shallow'
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header/>)
+    const wrapper = shallow(<Header startLogout={() => {}}/>)
     expect(wrapper).toMatchSnapshot()
     // expect(toJSON(wrapper)).toMatchSnapshot()
     // expect(wrapper.find('h1').length).toBe(1)
     // const renderer = new ReactShallowRenderer()
     // renderer.render(<Header/>)
     // expect(renderer.getRenderOutput()).toMatchSnapshot() // will always pass on first run b/c there is not existing snapshot
+})
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn()
+    const wrapper = shallow(<Header startLogout={startLogout}/>)
+    wrapper.find('button').simulate('click')
+    expect(startLogout).toHaveBeenCalled()
 })
 
 // react-test-renderer
